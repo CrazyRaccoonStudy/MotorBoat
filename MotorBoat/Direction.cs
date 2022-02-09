7f7c8c51ce0860f1ba5ec89d5d8b6b633fbc85d6
Malde <Malde@RACCOON>
2022-02-10 00:31:15 +0400

    Добавьте файлы проекта.

---
+++ MotorBoat/Direction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorBoat
+{
+    /// <summary>
+    /// Направление движения автомобиля
+    /// </summary>
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
